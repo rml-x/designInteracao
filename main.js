f633a557e16753bbbe1b7bcfefb80a1fb46508a2
rml-x <cristiane.xavier@aluno.riogrande.ifrs.edu.br>
2026-03-20 15:16:25 -0300

    function and image

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function carregarHeader() {
     fetch('header.html')
         .then(response => response.text())
         .then(data => {
-            document.getElementById('header').innerHTML = data;
+            document.querySelector('.header').innerHTML = data;
         });
 }
 
@@ -10,8 +10,115 @@ function carregarFooter() {
     fetch('footer.html')
         .then(response => response.text())
         .then(data => {
-            document.getElementById('footer').innerHTML = data;
+            document.querySelector('.footer').innerHTML = data;
         });
+}
+
+function adicionarQuadrado(){
+    let textoQuadrado = document.querySelector("#texto").value;
+    let novoQuadrado = document.createElement('div');
+    novoQuadrado.className = 'quadrado';
+    novoQuadrado.textContent = textoQuadrado;
+    novoQuadrado.style.color = selectedColor;
+    novoQuadrado.style.borderColor = selectedColorSquare;
+    painel.appendChild(novoQuadrado);
+}
+function removerQuadrado(){
+    let todosQuadrados = document.querySelectorAll(".quadrado");
+    let quadradoEscolhido = parseInt(document.querySelector("#escolhido").value)-1;
+    if (todosQuadrados[quadradoEscolhido]) {
+        todosQuadrados[quadradoEscolhido].remove()
+    }
+}
+function alterarFonte(){
+    let todosQuadrados = document.querySelectorAll(".quadrado");
+    let quadradoEscolhido = parseInt(document.querySelector("#escolhido").value)-1;
+    if (todosQuadrados[quadradoEscolhido]) {
+        todosQuadrados[quadradoEscolhido].style.fontSize = document.querySelector("#fontSizeButton").value + "px";
+    }
+}
+
+function alterarQuadrado(){
+    let todosQuadrados = document.querySelectorAll(".quadrado");
+    let quadradoEscolhido = parseInt(document.querySelector("#escolhido").value)-1;
+    if (todosQuadrados[quadradoEscolhido]) {
+        const tamanho = document.querySelector("#SquareSizeButton").value;
+        todosQuadrados[quadradoEscolhido].style.width = tamanho + "px";
+        todosQuadrados[quadradoEscolhido].style.height = tamanho + "px";
+    }
+}
+
+function alterarCorQuadrado(){
+    selectedColorSquare = document.querySelector("#corquadrado").value;
+}
+function alterarCorFonte(){
+    selectedColor = document.querySelector("#corfonte").value;
+}
+
+function uploadImageFunc(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function(e) {
+            if (!cardImageElement) {
+                cardImageElement = document.createElement('img');
+                card.appendChild(cardImageElement);
+            }
+            cardImageElement.src = e.target.result;
+            cardImageElement.style.width = '50%';    
+        };  
+    }
+}
+
+function removeImageFunc() {
+    if (cardImageElement) {
+        card.removeChild(cardImageElement);
+        cardImageElement = null;
+        uploadImageButton.value = '';
+    }
+}
+
+//Main
+const card = document.querySelector('#cardId');
+const uploadImageButton = document.querySelector('#selectImageId');
+const removeImageButton = document.querySelector('#removeImageId');
+let cardImageElement = null;
+
+const painel = document.querySelector('#painel');
+const adicionarButton = document.querySelector('#adicionarButton');
+const removerButton = document.querySelector('#removerButton');
+const fontSizeButton = document.querySelector('#fontSizeButton');
+const squareSizeButton = document.querySelector('#SquareSizeButton');
+const corfonte = document.querySelector('#corfonte');
+const corquadrado = document.querySelector('#corquadrado');
+
+let selectedColor = '#000000';
+let selectedColorSquare ='#000000';
+
+if (uploadImageButton) {
+    uploadImageButton.addEventListener('change', uploadImageFunc);
+}
+if (removeImageButton) {
+    removeImageButton.addEventListener('click', removeImageFunc);
+}
+if (adicionarButton) {
+    adicionarButton.addEventListener('click', adicionarQuadrado);
+}
+if (removerButton) {
+    removerButton.addEventListener('click', removerQuadrado);
+}
+if (fontSizeButton) {
+    fontSizeButton.addEventListener('input', alterarFonte);
+}
+if (squareSizeButton) {
+    squareSizeButton.addEventListener('input', alterarQuadrado);
+}
+if (corfonte) {
+    corfonte.addEventListener('input', alterarCorFonte);
+}
+if (corquadrado) {
+    corquadrado.addEventListener('input', alterarCorQuadrado);
 }
 
 window.onload = () => {
