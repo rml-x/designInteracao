d23ce60f9b7d29403f9689f653189d6c9b87a859
rml-x <cristiane.xavier@aluno.riogrande.ifrs.edu.br>
2026-06-23 18:34:20 -0300

    add: fetch json

--- js/prova.js
+++ js/prova.js
@@ -1,43 +1,4 @@
 
-const prova = {
-
-    titulo: "Prova de Design de Interação",
-
-    questoes: [
-        {
-            enunciado: "O que é affordance?",
-            alternativas: [
-                "A cor predominante de uma interface",
-                "A propriedade de um objeto que sugere como ele deve ser usado",
-                "Um tipo de fonte tipográfica"
-            ],
-            correta: 1  
-        },
-
-        {
-            enunciado: "Assinale a alternativa que apresenta uma forma utilizada na avaliação preditiva para avaliar a usabilidade de uma interface: ",
-            alternativas: [
-                "Realização de conversas informais",
-                "Utilização de heurísticas",
-                "Prototipação"
-            ],
-            correta: 1  
-        },
-
-        {
-            enunciado: "Assinale a alternativa que NÃO apresenta uma melhoria de usabilidade na web proporcionada pela evolução tecnológica: ",
-            alternativas: [
-                "Melhoria na velocidade de downloads, devido ao advento de tecnologias de banda larga.",
-                "Melhorias nos navegadores, que “aprenderam” a imprimir frames e a fazer funcionar o botão “voltar”.",
-                "Adequação de conteúdos ao ambiente web, tornando os textos menores e a leitura mais dinâmica. "
-            ],
-            correta: 2 
-        },
-
-        
-    ]
-};
-
 function gerarQuestaoHTML(questao, indice) {
 
     const alternativasHTML = questao.alternativas.map((alt, i) => `
@@ -55,170 +16,183 @@ function gerarQuestaoHTML(questao, indice) {
     `;
 }
 
-const todasQuestoesHTML = prova.questoes.map((q, i) => 
-    gerarQuestaoHTML(q, i)
-).join('');
-
-
-
-
 
 class MinhaProva extends HTMLElement {
 
-    constructor() {
-
-        super();
-        const shadow = this.attachShadow({ mode: 'open' });
-
+    async carregarProva() {
         
-        shadow.innerHTML = `
+        try {
+             
+            const response = await fetch('prova.json');   
 
-            <style>
-
-
-
-            :host {
-                display: block;
-                width: 100%;
-                box-sizing: border-box;
-                padding-bottom: 80px;
+            if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
             }
 
-            p, h2, label, #resultado {
-                display: block;
-                margin: 5px 0;
-                padding: 0px 14px;
-            }
+            const prova = await response.json();
+            console.log(prova);
 
-            #corrigir {
-                margin: 14px auto;
-                width: 100%;
-                padding: 12px;
-                background: var(--primary);
-                color: white;
-                border: none;
-                border-radius: 6px;
-                cursor: pointer;
-                margin-top: 20px;
-                font-weight: bold;
-                transition: background 0.2s;
-                margin-bottom: 40px;
-            }
+            const todasQuestoesHTML = prova.questoes.map((q, i) => gerarQuestaoHTML(q, i)).join('');
 
+            this.shadow.innerHTML = `
 
-            #corrigir:hover { background: #1d4ed8; }
+                <style>
 
-            #reiniciar {
-
-                margin: 14px auto;
-                width: 100%;
-                padding: 12px;
-                color: black;
-                border: none;
-                border-radius: 6px;
-                cursor: pointer;
-                margin-top: 20px;
-                font-weight: bold;
-                transition: background 0.2s;
-            }
-
-            #resultado{
-                padding: 10px;
-            }
-
-            @media (max-width: 768px) {
-
-                 :host {
+                :host {
                     display: block;
                     width: 100%;
-                }
-
-                p, h2, label {
-                    font-size: 18px; 
-                }
-
-                .questao {
-                    width: 100%; 
                     box-sizing: border-box;
+                    padding-bottom: 80px;
                 }
 
-                #corrigir, #reiniciar {
-                    box-sizing: border-box;
-                    max-width: 100%;
-                }
-            }
-
-            </style>
-            
-
-            <h2>${prova.titulo}</h2>
-
-            ${todasQuestoesHTML}
-
-            <br>
-            <button id = "corrigir" >Corrigir</button>
-
-            <div id="resultado"></div>
-        `
-
-        shadow.querySelector('button').addEventListener('click', () => {
-
-            let acertos = 0;
-            let resultadoHTML = '';
-
-            const resultado = shadow.querySelector('#resultado');
-
-            prova.questoes.forEach((questao, i) => {
-
-                const marcado = shadow.querySelector(`input[name="q${i}"]:checked`);
-
-                if (marcado && Number(marcado.value) === questao.correta) {
-                    acertos = acertos + 1;
+                p, h2, label, #resultado {
+                    display: block;
+                    margin: 5px 0;
+                    padding: 0px 14px;
                 }
 
-                 const respostaUsuario = marcado ? questao.alternativas[marcado.value] : "Não respondida";
-                 const respostaCorreta = questao.alternativas[questao.correta];
-
-                 resultadoHTML += `
-                    <div class="item-resultado">
-                        <p><strong>${questao.enunciado}</strong></p>
-                        <p>Sua resposta: ${respostaUsuario}</p>
-                        <p>Resposta correta: ${respostaCorreta}</p>
-                    </div>
-                    `;
-
-                 
-
-            });
-
-            let responderNovamente = `<button id="reiniciar">Responder novamente</button>`;
-
-            
-
-            if (acertos === prova.questoes.length) {
-                resultado.innerHTML = `<p>Você acertou todas as questões!</p> 
-                <p>Correção: </p>` +  responderNovamente + resultadoHTML;
+                #corrigir {
+                    margin: 14px auto;
+                    width: 100%;
+                    padding: 12px;
+                    background: var(--primary);
+                    color: white;
+                    border: none;
+                    border-radius: 6px;
+                    cursor: pointer;
+                    margin-top: 20px;
+                    font-weight: bold;
+                    transition: background 0.2s;
+                    margin-bottom: 40px;
+                }
 
 
-            }else { 
-                resultado.innerHTML =  `<p>Você acertou ${acertos} de ${prova.questoes.length} questões!</p>  
-                <p>Correção: </p>`+  responderNovamente + resultadoHTML;
+                #corrigir:hover { background: #1d4ed8; }
 
-            }
+                #reiniciar {
 
-            
-            shadow.querySelector('#reiniciar').addEventListener('click', () => {
+                    margin: 14px auto;
+                    width: 100%;
+                    padding: 12px;
+                    color: black;
+                    border: none;
+                    border-radius: 6px;
+                    cursor: pointer;
+                    margin-top: 20px;
+                    font-weight: bold;
+                    transition: background 0.2s;
+                }
 
-                shadow.querySelectorAll('input[type="radio"]').forEach(input => {
-                    input.checked = false;
-                });
+                #resultado{
+                    padding: 10px;
+                }
+
+                @media (max-width: 768px) {
+
+                    :host {
+                        display: block;
+                        width: 100%;
+                    }
+
+                    p, h2, label {
+                        font-size: 18px; 
+                    }
+
+                    .questao {
+                        width: 100%; 
+                        box-sizing: border-box;
+                    }
+
+                    #corrigir, #reiniciar {
+                        box-sizing: border-box;
+                        max-width: 100%;
+                    }
+                }
+
+                </style>
                 
-                resultado.innerHTML = '';
+
+                <h2>${prova.titulo}</h2>
+
+                ${todasQuestoesHTML}
+
+                <br>
+                <button id = "corrigir" >Corrigir</button>
+
+                <div id="resultado"></div>
+            `
+
+        
+
+            this.shadow.querySelector('button').addEventListener('click', () => {
+
+                let acertos = 0;
+                let resultadoHTML = '';
+
+                const resultado = this.shadow.querySelector('#resultado');
+
+                prova.questoes.forEach((questao, i) => {
+
+                    const marcado = this.shadow.querySelector(`input[name="q${i}"]:checked`);
+
+                    if (marcado && Number(marcado.value) === questao.correta) {
+                        acertos = acertos + 1;
+                    }
+
+                    const respostaUsuario = marcado ? questao.alternativas[marcado.value] : "Não respondida";
+                    const respostaCorreta = questao.alternativas[questao.correta];
+
+                    resultadoHTML += `
+                        <div class="item-resultado">
+                            <p><strong>${questao.enunciado}</strong></p>
+                            <p>Sua resposta: ${respostaUsuario}</p>
+                            <p>Resposta correta: ${respostaCorreta}</p>
+                        </div>
+                        `;
+
+                    
+
+                });
+
+                let responderNovamente = `<button id="reiniciar">Responder novamente</button>`;
+
+                
+
+                if (acertos === prova.questoes.length) {
+                    resultado.innerHTML = `<p>Você acertou todas as questões!</p> 
+                    <p>Correção: </p>` +  responderNovamente + resultadoHTML;
+
+
+                }else { 
+                    resultado.innerHTML =  `<p>Você acertou ${acertos} de ${prova.questoes.length} questões!</p>  
+                    <p>Correção: </p>`+  responderNovamente + resultadoHTML;
+
+                }
+
+                
+                this.shadow.querySelector('#reiniciar').addEventListener('click', () => {
+
+                    this.shadow.querySelectorAll('input[type="radio"]').forEach(input => {
+                        input.checked = false;
+                    });
+                    
+                    resultado.innerHTML = '';
+                });
+
+
             });
+            
+            
+        } 
+        catch (error) {
+            console.error('Fetch failed:', error);
+        }
+    }
 
-
-        });
-
+    constructor() {
+        super();
+        this.shadow = this.attachShadow({ mode: 'open' });
+        this.carregarProva();
     }
 }
 
