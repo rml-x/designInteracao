b1e5e6b897ef439123b8574f9870d102a00c2268
rml-x <cristiane.xavier@aluno.riogrande.ifrs.edu.br>
2026-06-25 01:56:54 -0300

    add: then/cath errors

--- js/APImet.js
+++ js/APImet.js
@@ -1,4 +1,13 @@
 
+// Recado: apesar de não estar nos requisitos
+// do trabalho que dizia para utilizar 1 api 
+// nessa pagina, quis fazer com que a experiencia
+// do usuario para pesquisar a cidade fosse mais 
+// facil entao optei por utilizar duas apis, uma para 
+// pesquisar a cidade por nome, que passa esse valor para
+// a proxima que entao traz as informações meteorologicas.
+// ao inves do usuario precisar introduzir o id da cidade.
+
 function setSearch(form) {
 
     const cityNameInput = document.getElementById("CityName");
@@ -13,8 +22,17 @@ function setSearch(form) {
             method: "GET",
         }
     )
-    .then(data => data.json())
+    .then(data => {
+        if (!data.ok) {
+            throw new Error(`error! Status: ${data.status}`);
+        }
+        return data.json();
+    })
     .then((json) => {
+       
+        if (json.length === 0) {
+            throw new Error("cidade não encontrada!!");
+        }
 
         const htmlTemplate= json.map( item => {
             return `
@@ -27,7 +45,10 @@ function setSearch(form) {
         const finalHtml = htmlTemplate.join(`<br>`);
         document.getElementById('cidade').innerHTML = finalHtml;
 
-    
+    })
+    .catch(error => {
+        console.error(error);
+        document.getElementById('cidade').innerHTML = `<p>Erro: ${error.message}</p>`;
     });
     return false;
 
@@ -50,9 +71,18 @@ function setAction(cityCode) {
             method: "GET",
         }
     )
-    .then(data => data.json())
+    .then(data => {
+        if (!data.ok) {
+            throw new Error(`error! Status: ${data.status}`);
+        }
+        return data.json();
+    })
     .then((json) => {
         document.getElementById('response').innerHTML = JSON.stringify(json);  
+    })
+    .catch(error => {
+        console.error(error);
+        document.getElementById('response').innerHTML = `<p>Erro: ${error.message}</p>`;
     });
     return false;
 }
