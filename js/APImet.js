ac967c4d7ba31b7ad0370d4b54656c549db880f2
rml-x <cristiane.xavier@aluno.riogrande.ifrs.edu.br>
2026-06-25 01:27:28 -0300

    add: city search, js function

--- js/APImet.js
+++ js/APImet.js
@@ -1,10 +1,43 @@
 
-function setAction(form) {   
+function setSearch(form) {
+
+    const cityNameInput = document.getElementById("CityName");
+    const CityName = cityNameInput.value;
+
+    const url = `https://brasilapi.com.br/api/cptec/v1/cidade/${CityName}`;
+
+    fetch(
+        url,
+        {            
+            headers: { "Content-Type": "application/json" },            
+            method: "GET",
+        }
+    )
+    .then(data => data.json())
+    .then((json) => {
+
+        const htmlTemplate= json.map( item => {
+            return `
+            <div class="cidade-card" onclick="setAction('${item.id}')">
+                <h3>${item.nome}, ${item.estado}, ${item.regiao}</h3>
+            </div>
+            `;
+        });
+
+        const finalHtml = htmlTemplate.join(`<br>`);
+        document.getElementById('cidade').innerHTML = finalHtml;
+
     
-    const cityCodeInput = document.getElementById("CityCode");
+    });
+    return false;
+
+}
+
+
+function setAction(cityCode) {   
+    
     const daysInput = document.getElementById("days");
 
-    const cityCode = cityCodeInput.value;
     const days = daysInput.value;
 
     const url = `https://brasilapi.com.br/api/cptec/v1/clima/previsao/${cityCode}/${days}`;
